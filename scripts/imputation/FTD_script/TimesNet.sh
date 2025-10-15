c0fd98a809379667823d777e4993c56193bf9ab5
JichengLi98 <jichengli@ln.hk>
2025-10-15 10:49:59 +0800

    Update TimesNet.sh

--- scripts/imputation/FTD_script/TimesNet.sh
+++ scripts/imputation/FTD_script/TimesNet.sh
@@ -1,1 +1,138 @@
+export CUDA_VISIBLE_DEVICES=0
 
+model_name=TimesNet
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/FTD/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.1 \
+  --mask_rate 0.1 \
+  --model $model_name \
+  --data FTD \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --batch_size 100 \
+  --d_model 64 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 3 \
+  --learning_rate 0.0005
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/FTD/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.2 \
+  --mask_rate 0.2 \
+  --model $model_name \
+  --data FTD \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --batch_size 100 \
+  --d_model 64 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 3 \
+  --learning_rate 0.0005
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/FTD/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.3 \
+  --mask_rate 0.3 \
+  --model $model_name \
+  --data FTD \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --batch_size 100 \
+  --d_model 64 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 3 \
+  --learning_rate 0.0005
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/FTD/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.4 \
+  --mask_rate 0.4 \
+  --model $model_name \
+  --data FTD \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --batch_size 100 \
+  --d_model 64 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 3 \
+  --learning_rate 0.0005
+
+  python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/FTD/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.5 \
+  --mask_rate 0.5 \
+  --model $model_name \
+  --data FTD \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --batch_size 100 \
+  --d_model 64 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 3 \
+  --learning_rate 0.0005
