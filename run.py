be78c82debd01fc8bfabb2f95bfd7a1b4bb0c3b8
JichengLi98 <jichengli@ln.hk>
2025-10-23 16:02:31 +0800

    Update run.py

--- run.py
+++ run.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
                         help='model name, options: [Autoformer, Transformer, TimesNet]')
 
     # data loader
-    parser.add_argument('--data', type=str, required=True, default='ETTh1', help='dataset type')
-    parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
-    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+    parser.add_argument('--data', type=str, required=True, default='FTD', help='dataset type')
+    parser.add_argument('--root_path', type=str, default='./dataet/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default='FTD.csv', help='data file')
     parser.add_argument('--features', type=str, default='M',
                         help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
     parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
