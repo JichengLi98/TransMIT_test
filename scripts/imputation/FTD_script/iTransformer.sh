69cdf2c268bc3b3e1f74cb7a0f971a451d741b94
JichengLi98 <jichengli@ln.hk>
2025-10-27 00:30:22 +0800

    Update iTransformer.sh

--- scripts/imputation/FTD_script/iTransformer.sh
+++ scripts/imputation/FTD_script/iTransformer.sh
@@ -5,14 +5,95 @@ model_name=iTransformer
 python -u run.py \
   --task_name imputation \
   --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_mask_0.125 \
-  --mask_rate 0.125 \
+  --root_path ./dataset/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.1 \
+  --mask_rate 0.1 \
   --model $model_name \
-  --data ETTh2 \
+  --data custom \
   --features M \
-  --seq_len 96 \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 89 \
+  --dec_in 89 \
+  --c_out 89 \
+  --batch_size 100 \
+  --d_model 128 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 5 \
+  --learning_rate 0.0005
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.2 \
+  --mask_rate 0.2 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 89 \
+  --dec_in 89 \
+  --c_out 89 \
+  --batch_size 100 \
+  --d_model 128 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 5 \
+  --learning_rate 0.0005
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.3 \
+  --mask_rate 0.3 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 16 \
+  --label_len 0 \
+  --pred_len 0 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 89 \
+  --dec_in 89 \
+  --c_out 89 \
+  --batch_size 100 \
+  --d_model 128 \
+  --d_ff 128 \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 5 \
+  --learning_rate 0.0005
+
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.4 \
+  --mask_rate 0.4 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 16 \
   --label_len 0 \
   --pred_len 0 \
   --e_layers 2 \
@@ -21,79 +102,25 @@ python -u run.py \
   --enc_in 7 \
   --dec_in 7 \
   --c_out 7 \
-  --batch_size 16 \
+  --batch_size 100 \
   --d_model 128 \
   --d_ff 128 \
   --des 'Exp' \
   --itr 1 \
   --top_k 5 \
-  --learning_rate 0.001
+  --learning_rate 0.0005
 
 python -u run.py \
   --task_name imputation \
   --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_mask_0.25 \
-  --mask_rate 0.25 \
-  --model $model_name \
-  --data ETTh2 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 128 \
-  --d_ff 128 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_mask_0.375 \
-  --mask_rate 0.375 \
-  --model $model_name \
-  --data ETTh2 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 128 \
-  --d_ff 128 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh2.csv \
-  --model_id ETTh2_mask_0.5 \
+  --root_path ./dataset/ \
+  --data_path FTD.csv \
+  --model_id FTD_mask_0.5 \
   --mask_rate 0.5 \
   --model $model_name \
-  --data ETTh2 \
+  --data custom \
   --features M \
-  --seq_len 96 \
+  --seq_len 16 \
   --label_len 0 \
   --pred_len 0 \
   --e_layers 2 \
@@ -102,10 +129,11 @@ python -u run.py \
   --enc_in 7 \
   --dec_in 7 \
   --c_out 7 \
-  --batch_size 16 \
+  --batch_size 100 \
   --d_model 128 \
   --d_ff 128 \
   --des 'Exp' \
   --itr 1 \
   --top_k 5 \
-  --learning_rate 0.001
+  --learning_rate 0.0005
+  
