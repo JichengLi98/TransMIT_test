43d8b646e04f2ae709498655f65592f6ad8956e1
JichengLi98 <jichengli@ln.hk>
2025-10-28 10:10:38 +0800

    Create TransMIT.py

---
+++ models/TransMIT.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.Transformer_EncDec import Decoder, DecoderLayer, Encoder, EncoderLayer, ConvLayer
+from layers.SelfAttention_Family import FullAttention, AttentionLayer
+from layers.Embed import DataEmbedding, DataEmbedding_inverted
+import numpy as np
+
+class Model(nn.Module):
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.task_name = configs.task_name
+        self.pred_len = configs.pred_len
+        # Embedding 
+        self.enc_embedding = DataEmbedding(configs.enc_in, configs.d_model, configs.embed, configs.freq,
+                                           configs.dropout)
+        self.enc_embedding_inverted = DataEmbedding_inverted(configs.enc_in, configs.d_model, configs.embed, configs.freq,
+                                           configs.dropout)
+
+        # Encoder    
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=False), configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        ) 
+        self.encoder_inverted = Encoder(
+            [ 
+                EncoderLayer(
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=False), configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        )
+        # Decoder
+        if self.task_name == 'imputation':
+            self.projection = nn.Linear(2*configs.d_model, configs.c_out, bias=True)
+
+    def imputation(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask):
+        # Embedding
+        enc_out1 = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out2 = self.enc_embedding_inverted(x_enc, x_mark_enc)
+      
+        enc_out1, attns1 = self.encoder(enc_out1, attn_mask=None)
+        enc_out2, attns2 = self.encoder_inverted(enc_out2, attn_mask=None)
+        enc_out2 = nn.Linear(configs.d_model, configs.seq_len, bias=True)(enc_out2)
+        enc_out2 = enc_out2.permute(0, 2, 1)
+
+        enc_out = torch.cat([enc_out1, enc_out2], 1)
+
+        dec_out = self.projection(enc_out)
+        return dec_out
+
+
+
