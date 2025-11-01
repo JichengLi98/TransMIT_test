9e835f1324490f90ba474a17cda78549c0e81b11
JichengLi98 <jichengli@ln.hk>
2025-11-01 11:15:50 +0800

    Update run.py

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--fix_seed', type=int, default=2025)
     # basic config
-    parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast',
+    parser.add_argument('--task_name', type=str, required=True, default='imputation',
                         help='task name, options:[long_term_forecast, short_term_forecast, imputation, classification, anomaly_detection]')
     parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
     parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
